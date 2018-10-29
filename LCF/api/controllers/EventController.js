3a38146eab7bff385916d705e29a67421a6c633c
18445756 <lichaofan@lichaofandeMacBook-Pro.local>
2018-10-29 19:31:45 +0800

    Initial commit

---
+++ LCF/api/controllers/EventController.js
@@ -0,0 +1,154 @@
+/**
+ * EventController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    // action - create
+    create: async function (req, res) {
+
+        if (req.method == "GET")
+            return res.view('event/create');
+
+        if (typeof req.body.Event === "undefined")
+            return res.badRequest("Form-data not received.");
+
+        await Event.create(req.body.Event);
+
+        return res.ok("Successfully created!");
+    },
+
+    // action - index
+    index: async function (req, res) {
+
+        var models = await Event.find();
+        return res.view('event/index', { events: models });
+
+    },
+
+    // action - first
+    first: async function (req, res) {
+
+        var models = await Event.find();
+        return res.view('event/first', { events: models });
+
+    },
+
+    // action - details
+    details: async function (req, res) {
+
+        var models = await Event.find();
+        return res.view('event/details', { events: models });
+
+    },
+
+    // action - view
+    view: async function (req, res) {
+
+        var message = Event.getInvalidIdMsg(req.params);
+
+        if (message) return res.badRequest(message);
+
+        var model = await Event.findOne(req.params.id);
+
+        if (!model) return res.notFound();
+
+        return res.view('event/view', { event: model });
+
+    },
+
+    // action - delete 
+    delete: async function (req, res) {
+
+        if (req.method == "GET") return res.forbidden();
+
+        var message = Event.getInvalidIdMsg(req.params);
+
+        if (message) return res.badRequest(message);
+
+        var models = await Event.destroy(req.params.id).fetch();
+
+        if (models.length == 0) return res.notFound();
+
+        return res.ok("Event Deleted.");
+
+    },
+
+    // action - update
+    update: async function (req, res) {
+
+        var message = Event.getInvalidIdMsg(req.params);
+
+        if (message) return res.badRequest(message);
+
+        if (req.method == "GET") {
+
+            var model = await Event.findOne(req.params.id);
+
+            if (!model) return res.notFound();
+
+            return res.view('event/update', { event: model });
+
+        } else {
+
+            if (typeof req.body.Event === "undefined")
+                return res.badRequest("Form-data not received.");
+
+            var models = await Event.update(req.params.id).set({
+                name: req.body.Event.name,
+                age: req.body.Event.age
+            }).fetch();
+
+            if (models.length == 0) return res.notFound();
+
+            return res.ok("Record updated");
+
+        }
+    },
+
+    // search function
+    search: async function (req, res) {
+
+        const qName = req.query.name || "";
+        const qAge = parseInt(req.query.age);
+
+        if (isNaN(qAge)) {
+
+            var models = await Event.find({
+                where: { name: { contains: qName } },
+                sort: 'name'
+            });
+
+        } else {
+
+            var models = await Event.find({
+                where: { name: { contains: qName }, age: qAge },
+                sort: 'name'
+            });
+
+        }
+
+        return res.view('event/index', { events: models });
+    },
+
+    // action - paginate
+    paginate: async function (req, res) {
+
+        const qPage = Math.max(req.query.page - 1, 0) || 0;
+
+        const numOfItemsPerPage = 2;
+
+        var models = await Event.find({
+            limit: numOfItemsPerPage,
+            skip: numOfItemsPerPage * qPage
+        });
+
+        var numOfPage = Math.ceil(await Event.count() / numOfItemsPerPage);
+
+        return res.view('event/paginate', { events: models, count: numOfPage });
+    },
+
+};
+
